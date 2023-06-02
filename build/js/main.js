ef4d7b5d599e831b700e392513d1fc5d65aa234b
zmhp <manashahp1995@gmail.com>
2023-06-03 00:42:56 +0530

    practice TS basics

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,16 @@
 "use strict";
 // ********************************************************************************
+// ********************************************************************************
+// ********************************************************************************
+// COMMANDS
+// npm install typescript -g // INSTALL TS GLOBALLY
+// tsc main.ts main.js -w // EXECUTE TS
+// tsc src/main.ts build/js/main.js -w // EXECUTE TS
+// tsc --init // SETUP CONFIG FILE , SET ROOTDIR AND OUTDIR
+// tsc -w // IF THE CONFIG FILE IS SET UP
+// ********************************************************************************
+// ********************************************************************************
+// ********************************************************************************
 // let a = 10
 // let b = '25'
 // let c = true
@@ -7,4 +18,14 @@
 // console.log(a/c) // ERROR
 // console.log(a+b); //  1025
 // console.log(a+c); // ERROR
+// ********************************************************************************
+// let a:  number = 10;
+// let b:  string = '25'
+// let c:  boolean = true
+// console.log(a+b); // 1025
+// console.log(a+c); // ERROR
+// console.log(b+c); // 25true
+// console.log(a/b); // ERROR
+// console.log(b/c); // ERROR
+// console.log(a/c); // ERROR
 // ********************************************************************************
