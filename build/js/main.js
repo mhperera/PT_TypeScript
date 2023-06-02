c1c1b205c2f02e13674fe8c468c2a0f8f2acc4cf
zmhp <manashahp1995@gmail.com>
2023-06-03 01:19:43 +0530

    union type, regExp type, function and types

--- build/js/main.js
+++ build/js/main.js
@@ -29,3 +29,49 @@
 // console.log(b/c); // ERROR
 // console.log(a/c); // ERROR
 // ********************************************************************************
+// let a = "manasha"
+// a=42 // ERROR
+// let a:string = "manasha"
+// a=42 // ERROR
+// ********************************************************************************
+// let myName: string;
+// let age: number;
+// let isLoading: boolean;
+// let album: any;
+// myName = 'Manasha'; // OK
+// myName = 50         // ERROR
+// age = 40;           // OK
+// age = '40'          // ERROR
+// isLoading = true    // OK
+// isLoading = 1       // ERROR
+// isLoading = 0       // ERROR
+// album = 50          // OK
+// album = 'Tom Cat'   // OK
+// album = false       // OK
+// ********************************************************************************
+// let isLoading: number | boolean
+// isLoading = true
+// isLoading = false
+// isLoading = 1
+// isLoading = 0
+// ********************************************************************************
+// a dnd b are any typed
+// const sum  = (a, b) => {
+//     return a + b;
+// }
+// returns number for sure
+// const sum  = (a:number, b:number) => {
+//     return a + b;
+// }
+// returns string for sure
+// const sum  = (a:string, b:number) => {
+//     return a + b;
+// }
+// a dnd b are any typed , return number
+// const multiply  = (a, b) => {
+//     return a * b;
+// }
+// ********************************************************************************
+// let reg1 = /\w+\g
+// let reg2:RegExp = /\w+\g
+// ********************************************************************************
